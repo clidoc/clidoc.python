3bdac73af7f48efe4291c675c2d1d6483bb47ce5
haoxun <programmer.zhx@gmail.com>
2015-05-10 13:18:06 +0800

    Init.

---
+++ README.md
@@ -0,0 +1,3 @@
+`clidoc.python` is a python 2/3 codegen component of [clidoc][1].
+
+[1]: https://github.com/clidoc/clidoc
---
+++ clidoc/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'clidoc'
+project = "clidoc"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = ''
+authors = ['Zhan Haoxun']
+authors_string = ', '.join(authors)
+emails = ['Zhan Haoxun']
+license = 'MIT'
+copyright = '2015 ' + authors_string
+url = ''
